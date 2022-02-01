bf5857dc6201e6cf37d542554bbaa0808c105f80
Gulsen_Sohret <93159111+Gulsen927@users.noreply.github.com>
2022-02-01 14:46:57 -0500

    Test Tesla is done

--- src/test/java/com/cydeo/tests/day_01_selenium_intro/BasicNavigations.java
+++ src/test/java/com/cydeo/tests/day_01_selenium_intro/BasicNavigations.java
@@ -1,8 +1,25 @@
 package com.cydeo.tests.day_01_selenium_intro;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BasicNavigations {
+    public static void main(String[] args) {
 
-    //1- Setup the browser driver
+
+        //1- Setup the browser driver"
+
+        WebDriverManager.chromedriver().setup();
+
+        //2-Create instance of the Selenium Webdriver
+        //This is the line opening an empty browser
+
+        WebDriver driver=new ChromeDriver();
+
+        //3-Go to "https://www.tesla.com"
+        driver.get("https://www.tesla.com");
 
 
+    }
 }
